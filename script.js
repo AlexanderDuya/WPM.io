9fcccc0a8acb66a625f68d055459c68cb91c970e
AlexanderDuya <alexduyaconnect@gmail.com>
2025-06-16 21:58:48 +0100

    handled spacebar

--- script.js
+++ script.js
@@ -30,22 +30,64 @@ function newGame() {
   }
   addClass(document.querySelector(".word"), "current");
   addClass(document.querySelector(".letter"), "current");
+  positionCursor(); // ← here
 }
+
 document.getElementById("game").addEventListener("keyup", (e) => {
   const key = e.key;
   const currentLetter = document.querySelector(".letter.current");
-  const expected = currentLetter.innerHTML;
+  const expected = currentLetter?.innerHTML || " ";
   const isLetter = key.length === 1 && key !== " ";
-  console.log({ key, expected });
+  const isSpace = key === " ";
+  const currentWord = document.querySelector(".word.current");
 
   if (isLetter) {
     if (currentLetter) {
-      //alert(key === expected ? "ok" : "wrong");
       addClass(currentLetter, key === expected ? " correct" : "incorrect");
       removeClass(currentLetter, "current");
-      addClass(currentLetter.nextSibling, "current");
+      if (currentLetter.nextSibling) {
+        addClass(currentLetter.nextSibling, "current");
+      }
+      positionCursor();
+    } else {
+      const incorrectLetter = document.createElement("span");
+      incorrectLetter.innerHTML = key;
+      incorrectLetter.className = "letter incorrect extra";
+      currentWord.appendChild(incorrectLetter);
     }
   }
+
+  if (isSpace) {
+    if (expected !== " ") {
+      const lettersToInvalidate = [
+        ...document.querySelectorAll(".word.current .letter:not(.correct)"),
+      ];
+
+      lettersToInvalidate.forEach((letter) => {
+        addClass(letter, "incorrect");
+      });
+    }
+    removeClass(currentWord, "current");
+    addClass(currentWord.nextSibling, "current");
+    if (currentLetter) {
+      removeClass(currentLetter, "current");
+    }
+    addClass(currentWord.nextSibling.firstChild, "current");
+  }
 });
+
+function positionCursor() {
+  const currentLetter = document.querySelector(".letter.current");
+  const game = document.getElementById("game");
+  const cursor = document.getElementById("cursor");
+
+  if (currentLetter && cursor && game) {
+    const rect = currentLetter.getBoundingClientRect();
+    const gameRect = game.getBoundingClientRect();
+
+    cursor.style.top = `${rect.top - gameRect.top}px`;
+    cursor.style.left = `${rect.left - gameRect.left}px`;
+  }
+}
 
 newGame();
